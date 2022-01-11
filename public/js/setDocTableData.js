057bdb19a36bb067cb2938419d37fec0a0015d50
Benjie <61066771+BenjieEdroso@users.noreply.github.com>
2022-01-12 02:20:18 +0800

    add location

--- public/js/setDocTableData.js
+++ public/js/setDocTableData.js
@@ -3,32 +3,39 @@ export function setDocTableData() {
     fetch("http://localhost/DAMS/files/getFileInfo")
         .then((response) => response.json())
         .then((data) => {
-            let table = document.querySelector(".table-body");
+                let table = document.querySelector(".table-body");
 
-            //do this as many times as the database tables row length
-            data.forEach((record, ind, arr) => {
-                if (record.file_name.length > 27) {
-                    let tempName = record.file_name.substring(0, 27);
-                    record.file_name = tempName + "...";
-                }
-                let tableRowTemplate = `
+                //do this as many times as the database tables row length
+                data.forEach((record, ind, arr) => {
+                            let smallName = "";
+                            if (record.file_name.length > 27) {
+                                let tempName = record.file_name.substring(0, 27);
+                                smallName = tempName + "...";
+                            }
+                            let tableRowTemplate = `
             <tr>
                 <td class="file_id">${record.file_id}</td>
-                <td class="file_name">${record.file_name}</td>
+                <td class="file_name">${smallName}</td>
                 <td class="file_type">${record.file_type}</td>
                 <td class="file_tmpName">${record.file_tmpName}</td>
                 <td class="file_error">${record.file_error}</td>
                 <td class="file_size">${record.file_size}</td>
                 <td class="file_date">${record.file_date} </td>
                 <td class="file_actions">
-                    <a href="#"><i class="bi bi-file-earmark-arrow-down-fill"></i></a>
-                    <a href="#"><i class="bi bi-pencil-fill"></i></a>
+                    <a href="${
+                      location.origin +
+                      `/DAMS/files/download?fileName=${record.file_name}`
+                    }" class="download"><i class="bi bi-file-earmark-arrow-down-fill"></i></a>
+                    <a href="${
+                      location.origin +
+                      `/DAMS/files/edit?fileName=${record.file_name}`
+                    }"><i class="bi bi-pencil-fill"></i></a>
                     <a href="#"><i class="bi bi-folder-symlink-fill"></i></a>
                     <a href="#"><i class="bi bi-trash-fill"></i></a>
                 </td>
             </tr>`;
 
-                table.insertAdjacentHTML("beforeend", tableRowTemplate);
-            });
-        });
+        table.insertAdjacentHTML("beforeend", tableRowTemplate);
+      });
+    });
 }
