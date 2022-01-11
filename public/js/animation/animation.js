80edbbc3327252226cf8c839c7383e941bf9aeb4
Benjie <61066771+BenjieEdroso@users.noreply.github.com>
2022-01-11 16:05:01 +0800

    restart js folder

--- public/js/animation/animation.js
+++
@@ -1,7 +0,0 @@
-function removeSuccessAlert() {
-  setTimeout(() => {
-    $(".alert-dis").fadeOut("slow");
-  }, 2000);
-}
-
-removeSuccessAlert();
--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,6 @@
-import { asyncRequest, insertDataToElement } from "./functions.js";
-import { documentsRow } from "./template.js";
-
-let response;
-asyncRequest("GET", "http://localhost/DAMS/uploads/getFileInfo").then(
-  (responseData) => {
-    for (let i = 0; i < responseData.length; i++) {
-      document
-        .querySelector(".table-body")
-        .insertAdjacentHTML("beforeend", documentsRow());
-    }
-
-    let colName = Object.keys(responseData[0]);
-    for (var i = 0; i < colName.length; i++) {
-      insertDataToElement(colName[i], responseData);
-    }
-    response = responseData;
-  }
+//query sql to dbms return the data
+fetch("http://localhost/DAMS/files/getFileInfo").then((response) =>
+    response.json()
 );
-
-export default response;
+//create a table
+//insert those data in the table
