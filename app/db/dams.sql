56f264ae7df7d3b7d3dee7ee044ac43c53aff3e6
Benjie <61066771+BenjieEdroso@users.noreply.github.com>
2022-02-02 16:01:02 +0800

    new sql file

--- app/db/dams.sql
+++ app/db/dams.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2021 at 03:09 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.3.30
+-- Generation Time: Feb 02, 2022 at 08:59 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,72 @@ SET time_zone = "+00:00";
 --
 -- Database: `dams`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `setting_id` int(11) NOT NULL,
+  `setting_encypt` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_account`
+--
+
+CREATE TABLE `tbl_account` (
+  `account_id` int(11) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_account`
+--
+
+INSERT INTO `tbl_account` (`account_id`, `username`, `password`) VALUES
+(1, 'BenjieEdroso', '$2y$10$09N62amO9UxyKtBZvNYiIe2fyYjkwqiSJsoQBsu0IUd74QshvZD6e'),
+(2, 'something', '$2y$10$zXrhmnYNv6xO3LUdVrefC.gQvA7mfMRBM7ah.6tTuY0rRdqXnDhzC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_name`
+--
+
+CREATE TABLE `tbl_name` (
+  `name_id` int(11) NOT NULL,
+  `firstname` varchar(256) NOT NULL,
+  `middlename` varchar(256) NOT NULL,
+  `lastname` varchar(256) NOT NULL,
+  `account_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_name`
+--
+
+INSERT INTO `tbl_name` (`name_id`, `firstname`, `middlename`, `lastname`, `account_id`) VALUES
+(1, 'asd', 'asd', 'asd', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_student`
+--
+
+CREATE TABLE `tbl_student` (
+  `student_id` int(11) NOT NULL,
+  `name_id` int(11) NOT NULL,
+  `course` varchar(256) NOT NULL,
+  `year` int(1) NOT NULL,
+  `section` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -42,9 +108,10 @@ CREATE TABLE `uploads` (
 --
 
 INSERT INTO `uploads` (`file_id`, `file_name`, `file_type`, `file_tmpName`, `file_error`, `file_size`, `file_date`) VALUES
-(1, 'Emerging Trends in BPO.pptx', 'application/octet-stream', 'C:\\xampp\\tmp\\php5D6A.tmp', 0, 2081198, '2021-09-27 03:07:09'),
-(2, 'CCTS_Card.pdf', 'application/pdf', 'C:\\xampp\\tmp\\php8C5B.tmp', 0, 103957, '2021-09-27 03:07:21'),
-(3, 'doc.pdf', 'application/pdf', 'C:\\xampp\\tmp\\phpF51B.tmp', 0, 109025, '2021-09-27 03:14:21');
+(12, 'Capstone-Project-AY-2021-2022.pdf', 'application/pdf', 'C:\\xampp\\tmp\\phpBCC4.tmp', 0, 138290, '2022-01-29 07:24:53'),
+(13, 'Database design for mere mortals a hands-on guide to relational database design by Hernandez, Michael James (z-lib.org).epub.pdf', 'application/pdf', 'C:\\xampp\\tmp\\phpBCC5.tmp', 0, 12902513, '2022-01-29 07:24:53'),
+(14, 'Design Patterns- Elements of Reusable Object-Oriented Software ( PDFDrive ).pdf', 'application/pdf', 'C:\\xampp\\tmp\\phpBD62.tmp', 0, 2574928, '2022-01-29 07:24:53'),
+(15, 'Designing Data-Intensive Applications The Big Ideas Behind Reliable, Scalable, and Maintainable Systems by Martin Kleppmann (z-lib.org).pdf', 'application/pdf', 'C:\\xampp\\tmp\\phpBD92.tmp', 0, 24975901, '2022-01-29 07:24:53');
 
 -- --------------------------------------------------------
 
@@ -59,18 +126,34 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'test1', '$2y$10$iwDWF3wHmq5ZmgsPUn/KPeETWlH3EVbMFFIN4zf.yPP2TkW2nMPoW'),
-(2, 'test2', '$2y$10$a2VeuKLIr4nUva29yn4DvuZ6fdSqWemhpE2HopXQ/hBPDL1tBC.Sq'),
-(3, 'test3', '$2y$10$vsBrP/wRZF9o8IFR/nbmKOn3zgouFVsLPoOvEiDwLBuJVRYEFfegy'),
-(4, 'BenjieEdroso', '$2y$10$1R47mjZ1MUKVwwHvJ7T1O.a.v7dgk6dt27UZiDqFumU0MFHsvb/dy');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`setting_id`);
+
+--
+-- Indexes for table `tbl_account`
+--
+ALTER TABLE `tbl_account`
+  ADD PRIMARY KEY (`account_id`);
+
+--
+-- Indexes for table `tbl_name`
+--
+ALTER TABLE `tbl_name`
+  ADD PRIMARY KEY (`name_id`),
+  ADD UNIQUE KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`student_id`),
+  ADD UNIQUE KEY `name_id` (`name_id`);
 
 --
 -- Indexes for table `uploads`
@@ -89,16 +172,56 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `setting_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_account`
+--
+ALTER TABLE `tbl_account`
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_name`
+--
+ALTER TABLE `tbl_name`
+  MODIFY `name_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `uploads`
 --
 ALTER TABLE `uploads`
-  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_name`
+--
+ALTER TABLE `tbl_name`
+  ADD CONSTRAINT `tbl_name_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `tbl_account` (`account_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  ADD CONSTRAINT `tbl_student_ibfk_1` FOREIGN KEY (`name_id`) REFERENCES `tbl_name` (`name_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
