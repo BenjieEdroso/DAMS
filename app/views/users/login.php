10aca6b82b2d5ac6d3f4525390c31b953b2b4fce
Ronnel Dalisay <ronneldalisay18@gmail.com>
2022-02-02 22:12:28 -0800

    Design Login

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -2,18 +2,17 @@
 <!-- <img src="<?php echo URLROOT; ?>/images/chmsc_logo.png" alt="carlos hilado memorial state college logo" class=""
     style="height: 150px;"> -->
 <div class="side">
-    <img src="<?php echo URLROOT; ?>/images/chmsc-logo-side.png" class="side__logo"
+<img src="<?php echo URLROOT; ?>/images/chmsc.jpg" style="width: 44rem;height: 100vh; alt="carlos hilado memorial state college" >
+</div>
+<div class="center">
+    <img src="<?php echo URLROOT; ?>/images/chmsc-logo-side.png" class="center__logo"
         alt="carlos hilado memorial state college logo">
-    <h1 class="side__title">Document Archiving and Monitoring System</h1>
-    <div class="side__box side__box--border">
-        <h2 class="side__subtitle">this is h2</h2>
-    </div>
+    <h1 class="center__title">Document Archiving and Monitoring System</h1>
 </div>
 <form action="<?php echo URLROOT; ?>/users/login" method="post"
-    class="col-3 container  border position-absolute start-50 top-50 translate-middle rounded py-4 px-3">
+    class="col-3 container   position-absolute top-50 translate-middle rounded py-4 px-3" style="right:5%; bottom:45%;">
     <div class="mb3">
-        <h4>Sign in</h4>
-        <h1>ronnel</h1>
+        <h4 style="position:relative; left:40%;">Sign in</h4>
     </div>
     <div class="mb-4">
         <label for="exampleInputEmail1" class="form-label">Username</label>
@@ -33,13 +32,10 @@
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Remember me</label>
   </div> -->
-    <div class="mt-5">
+    <div class="mt-5" style="position: relative; left:27%;">
         <button type="submit" class="btn btn-primary">Sign in</button>
         <a class="btn btn-light" href="<?php echo URLROOT; ?>/users/register">Register</a>
     </div>
 </form>
-<a href="<?php echo URLROOT; ?>/users/register_student">Student Register</a>
-<a href="#">Dalisay</a>
-
 
 <!-- <?php require_once APPROOT . "/views/includes/footer.php"; ?> -->
