7983e0fea6260240f60fba3669d97426e5af7b4c
BenjieEdroso <ben09099@gmail.com>
2022-03-13 04:48:36 +0800

    finish small cards dashboard

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -93,9 +93,49 @@ if (!isset($_SESSION["username"]) && !isset($_SESSION["password"])) {
                 <span class="main__storage--left">Space Left <span>61%</span> </span>
             </div>
 
-            <div class="main__files"> </div>
-            <div class="main__users"> </div>
-            <div class="main__offices"> </div>
+            <div class="main__files">
+                <span class="main__files--label">Files</span>
+                <div class="main__files--flexCol">
+                    <span class="main__files--totalFiles"><span>1364</span> Files</span>
+                    <div class="main__files--flex">
+                        <span class="main__files--docx">1000 <img src="<?php echo URLROOT;?>/public/images/word.png"
+                                alt="word icon"> </span>
+                        <span class="main__files--pdf">364 <img src="<?php echo URLROOT;?>/public/images/pdf.png"
+                                alt="pdf icon"> </span>
+                    </div>
+                </div>
+            </div>
+            <div class="usersCard">
+                <div class="usersCard__label">Users</div>
+                <div class="usersCard--flex">
+                    <div class="usersCard__icon">
+                        <img src="<?php echo URLROOT;?>/public/images/user.png" alt="user icon">
+                    </div>
+                    <div class="usersCard__container">
+                        <div class="usersCard__data"> <span>6</span> <span
+                                class="usersCard__data--user">Faculties</span>
+                        </div>
+                        <div class="usersCard__data"> <span>5</span> <span class="usersCard__data--user">
+                                Admins</span></div>
+                        <div class="usersCard__data"> <span>16</span> <span
+                                class="usersCard__data--user">Teachers</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="office">
+                <div class="office__label">Office</div>
+                <div class="office__flexCont">
+                    <div class="office__flex">
+                        <div class="office__icon">
+                            <img src="<?php echo URLROOT;?>/public/images/office.png" alt="office icon">
+                        </div>
+
+                        <div class="office__data"><span class="office__data--number">13</span><span>Offices</span></div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </main>
 </div>
