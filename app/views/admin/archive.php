47c4c97fe1bbdee1683ceea554d99f76e6acbb9c
BenjieEdroso <ben09099@gmail.com>
2022-03-16 04:16:28 +0800

    add top nav archive

--- app/views/admin/archive.php
+++ app/views/admin/archive.php
@@ -7,6 +7,19 @@ if (!isset($_SESSION["username"]) && !isset($_SESSION["password"])) {
 }
 ?>
 <?php require_once APPROOT . "/views/includes/admin-sidebar.php"; ?>
-<?php require_once APPROOT . "/views/includes/admin-main.php"; ?>
+
+<main class="main main--archive">
+    <div class="topnav">
+        <div> <i class="material-icons icon">file_upload</i> Upload Files</div>
+        <div class="icon-scanner-cont"><i class="material-icons icon-scanner">scanner</i> Scan Files</div>
+        <form action="#" method="get">
+            <div> <i class="material-icons search-icon">search</i>
+                <input type="text" name="search" id="search" class="search-box">
+            </div>
+            <input type="submit" value="submit" hidden>
+        </form>
+    </div>
+</main>
+</div>
 <?php require_once APPROOT . "/views/includes/admin-main-footer.php"; ?>
 <?php require_once APPROOT . "/views/includes/footer.php"; ?>
