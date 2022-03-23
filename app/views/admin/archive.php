389af869512eb829cacd573ead431909100b63a8
benjie.edroso.pixel8 <benjie.edroso.pixel8@gmail.com>
2022-03-23 11:11:49 +0800

    fix control

--- app/views/admin/archive.php
+++ app/views/admin/archive.php
@@ -19,6 +19,44 @@ if (!isset($_SESSION["username"]) && !isset($_SESSION["password"])) {
             <input type="submit" value="submit" hidden>
         </form>
     </div>
+    <div class="table-card">
+        <ul>
+            <li><a href="#" class="archived">Archived</a></li>
+            <li><a href="#" class="retention">Retention</a></li>
+        </ul>
+        <div class="table-container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Code</th>
+                        <th>Name</th>
+                        <th>Date uploaded</th>
+                        <th>Last Modified</th>
+                        <th>Date to expire</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>FOM-1</td>
+                        <td>OJT-Form.docx</td>
+                        <td>03/19/2022</td>
+                        <td>03/20/2022</td>
+                        <td>04/20/2022</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>FOM-1</td>
+                        <td>OJT-Form.docx</td>
+                        <td>03/19/2022</td>
+                        <td>03/20/2022</td>
+                        <td>04/20/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </main>
 </div>
 <?php require_once APPROOT . "/views/includes/admin-main-footer.php"; ?>
--- app/views/admin/control.php
+++ app/views/admin/control.php
@@ -7,6 +7,199 @@ if (!isset($_SESSION["username"]) && !isset($_SESSION["password"])) {
 }
 ?>
 <?php require_once APPROOT . "/views/includes/admin-sidebar.php"; ?>
-<?php require_once APPROOT . "/views/includes/admin-main.php"; ?>
+<div class="control">
+    <div class="top-nav">
+        <a href="#" class="share-files"> <i class="material-icons send-icon">send</i> Share files to</a>
+        <form action="#" method="get">
+            <div class="search-container">
+                <i class="material-icons search-icon">search</i>
+                <input type="text" name="search" id="search" class="search">
+            </div>
+        </form>
+    </div>
+    <div class="top-links top-links-control">
+        <ul>
+            <li><a href="#">Recent</a></li>
+            <li><a href="#">All</a></li>
+            <li><span>Sort by:</span>
+                <select name="sort" id="sort">
+                    <option value="#">Date</option>
+                </select>
+            </li>
+
+        </ul>
+    </div>
+    <div class="control-cards">
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-word.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-pdf-box.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-word.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-pdf-box.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-word.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-pdf-box.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-word.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-pdf-box.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-word.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-pdf-box.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-word.png" alt="">
+            </div>
+        </div>
+        <div class="control-card">
+            <div class="card-text">
+                <span class="card-title">OJT Application Form</span>
+                <span class="card-shared">Share to: <span>Ms. Jane</span></span>
+                <span class="card-office">Office: <span>OSA</span></span>
+                <span class="card-access">Access until: <span>03/22/2022</span></span>
+                <span class="card-control">User can: <select name="control" id="control">
+                        <option value="Edit">Edit</option>
+                    </select></span>
+            </div>
+            <div>
+                <img src="<?php echo URLROOT?>/images/file-pdf-box.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php require_once APPROOT . "/views/includes/admin-main-footer.php"; ?>
 <?php require_once APPROOT . "/views/includes/footer.php"; ?>
