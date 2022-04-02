28d81c562201f5b3718754c298d44686198eedc2
BenjieEdroso <ben09099@gmail.com>
2022-04-02 23:54:18 +0800

    final cards top

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -1,30 +1,88 @@
 <?php require_once APPROOT . "/views/includes/header.php" ; ?>
 <div class="d-flex">
-    <aside class="bg-primary p-3 vh-100 col-2">
-        <ul class="list-group">
-            <li class="list-group-item  bg-primary border-0 p-0"><a href="#"
+    <aside class="sidebar p-3 vh-100 col-2">
+        <ul class="sidebar-links list-group">
+            <li class="list-group-item   border-0 p-0"><a href="#"
                     class="sidebar-link text-white p-auto text-decoration-none px-3 py-2 d-block"><i
                         class="bi bi-speedometer2 pe-3"></i>Dashboard</a></li>
-            <li class="list-group-item  bg-primary border-0 p-0"><a href="#"
+            <li class="list-group-item   border-0 p-0"><a href="#"
                     class="sidebar-link text-white p-auto text-decoration-none px-3 py-2 d-block">
                     <i class="bi bi-archive pe-3"></i>Archiving</a></li>
-            <li class="list-group-item  bg-primary border-0 p-0"><a href="#"
+            <li class="list-group-item   border-0 p-0"><a href="#"
                     class="sidebar-link text-white p-auto text-decoration-none px-3 py-2 d-block">
                     <i class="bi bi-activity pe-3"></i>Monitoring</a></li>
-            <li class="list-group-item  bg-primary border-0 p-0"><a href="#"
+            <li class="list-group-item   border-0 p-0"><a href="#"
                     class="sidebar-link text-white p-auto text-decoration-none px-3 py-2 d-block">
                     <i class="bi bi-send-plus pe-3"></i> Request</a></li>
-            <li class="list-group-item  bg-primary border-0 p-0"><a href="#"
+            <li class="list-group-item   border-0 p-0"><a href="#"
                     class="sidebar-link text-white p-auto text-decoration-none px-3 py-2 d-block">
                     <i class="bi bi-ui-radios pe-3"></i>Control</a></li>
-            <li class="list-group-item  bg-primary border-0 p-0"><a href="#"
+            <li class="list-group-item   border-0 p-0"><a href="#"
                     class="sidebar-link text-white p-auto text-decoration-none px-3 py-2 d-block">
                     <i class="bi bi-people pe-3"></i>User management</a>
             </li>
         </ul>
     </aside>
     <main class="col-10">
-
+        <div class="d-flex">
+            <div class="storage card col-3 px-4 py-3">
+                <div class="h5">Storage</div>
+                <div class="chart-size">
+                    <canvas id="myChart"></canvas>
+                </div>
+            </div>
+            <div class="files card col-3 px-4 py-3 ">
+                <div class="card-label h5">Files <i class="bi bi-file-earmark-text-fill card-icon"></i></div>
+                <div class="d-flex justify-content-between  my-auto">
+                    <div>
+                        <div class="number h4 ">1364</div>
+                        <div>Total</div>
+                    </div>
+                    <div>
+                        <div class="number h4 ">364</div>
+                        <div>docx</div>
+                    </div>
+                    <div>
+                        <div class="number h4 ">1000</div>
+                        <div>pdf</div>
+                    </div>
+                </div>
+            </div>
+            <div class="files card col-3 px-4 py-3 ">
+                <div class="card-label h5">Users <i class="bi bi-people-fill card-icon"></i></div>
+                <div class="d-flex justify-content-between  my-auto">
+                    <div>
+                        <div class="number h4 ">6</div>
+                        <div>Admin</div>
+                    </div>
+                    <div>
+                        <div class="number h4 ">8</div>
+                        <div>Staff</div>
+                    </div>
+                    <div>
+                        <div class="number h4 ">21</div>
+                        <div>Teachers</div>
+                    </div>
+                </div>
+            </div>
+            <div class="files card col-3 px-4 py-3 ">
+                <div class="card-label h5">Offices <i class="bi bi-building card-icon"></i></div>
+                <div class="d-flex justify-content-between  my-auto">
+                    <div>
+                        <div class="number h4 ">12</div>
+                        <div>Department</div>
+                    </div>
+                    <div>
+                        <div class="number h4 ">8</div>
+                        <div>Offices</div>
+                    </div>
+                    <div>
+                        <div class="number h4 ">16</div>
+                        <div>Buildings</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </div>
 <?php require_once APPROOT . "/views/includes/footer.php" ; ?>
--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,6 +1,7 @@
-    <script src="<?php echo URLROOT?>/js/jquery.min.js"></script>
-    <script src="<?php echo URLROOT?>/js/bootstrap.min.js"></script>
-    <script src="<?php echo URLROOT?>/js/main.js" type="module"></script>
-    </body>
+       <script src="<?php echo URLROOT?>/js/chart.min.js"></script>
+       <script src="<?php echo URLROOT?>/js/jquery.min.js"></script>
+       <script src="<?php echo URLROOT?>/js/bootstrap.min.js"></script>
+       <script src="<?php echo URLROOT?>/js/main.js" type="module"></script>
+       </body>
 
-    </html>
+       </html>
