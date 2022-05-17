d15209dbd1a8389c9bfe1b27a9971fe5db953e8f
BenjieEdroso <ben09099@gmail.com>
2022-05-17 22:32:52 +0800

    added new sidear

--- app/views/includes/dashboard-aside.php
+++ app/views/includes/dashboard-aside.php
@@ -1,18 +1,21 @@
      <aside class="sidebar p-3 vh-100 col-2 position-relative">
          <div class="d-flex align-items-center my-3">
-             <img src="<?php echo URLROOT?>/images/logo.png" alt="Carlos Hilado Memorial State College">
+             <img src="<?php echo URLROOT?>/images/chmsu-logo.png" width="80" height="80"
+                 alt="Carlos Hilado Memorial State College">
              <div>
                  <h5 class="text-white m-0">DAMS</h5>
-                 <p class=" m-0 small-text">Carlos Hilado Memorial State University</p>
+                 <p class="ms-2 small-text">Carlos Hilado Memorial State University</p>
              </div>
          </div>
          <ul class="nav flex-column ">
-             <li><a class="btn  text-white" href="<?php echo URLROOT?>/admin/dashboard">Dashboard</a></li>
-             <li><a class="btn  text-white" href="<?php echo URLROOT?>/admin/archiving">Archiving</a></li>
-             <li><a class="btn  text-white" href="<?php echo URLROOT?>/admin/monitoring">Monitoring</a></li>
-             <li><a class="btn  text-white" href="<?php echo URLROOT?>/admin/request">Request</a></li>
-             <li><a class="btn  text-white" href="<?php echo URLROOT?>/admin/control">Control</a></li>
-             <li><a class="btn  text-white" href="<?php echo URLROOT?>/admin/usermanagement">Management</a></li>
+             <li><a class="btn col-12" href="<?php echo URLROOT?>/admin/dashboard">Dashboard</a></li>
+             <li><a class="btn col-12" href="<?php echo URLROOT?>/admin/archiving">Archiving</a></li>
+             <li><a class="btn col-12" href="<?php echo URLROOT?>/admin/monitoring">Monitoring</a></li>
+             <li><a class="btn col-12" href="<?php echo URLROOT?>/admin/request">Request</a></li>
+             <li><a class="btn col-12" href="<?php echo URLROOT?>/admin/control">Control</a></li>
+             <li><a class="btn col-12" href="<?php echo URLROOT?>/admin/usermanagement">User
+                     Management</a>
+             </li>
          </ul>
          <a href="<?php echo URLROOT?>/users/signout" class="btn text-white position-absolute bottom-0">Logout</a>
      </aside>
